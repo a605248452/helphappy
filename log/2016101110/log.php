552eb548d54e9e9296b51cbedf885a6a158117ba
lzy <605248452@qq.com>
2016-10-11 10:44:10 +0800

    v1.0.1 author lzy

---
+++ log/2016101110/log.php
@@ -0,0 +1,1 @@
+2016-10-11 10:39:04"Controller:Index       Action:index"
---
+++ log/twig/4e/4ee57e4703a61dacd8a5d22aa76fbf87548da1bf62c298a3f0d124c0c15de9c7.php
@@ -0,0 +1,186 @@
+<?php
+
+/* index.html */
+class __TwigTemplate_a6da3ba12fceb827d59d5d5e40159782f83e69e6e972a3f03884777a7642ec10 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<!DOCTYPE html>
+<html lang=\"en\">
+
+<head>
+    <meta charset=\"utf-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <meta name=\"description\" content=\"\">
+    <meta name=\"author\" content=\"\">
+    <title>FAST首页</title>
+    <style>
+\t\tbody{
+\t\t\tbackground: rgba(220, 55, 55, 0.9);
+    \t\tbackground-size: 100% 100%;
+    \t\tfont-family: Trebuchet MS, snas-serif;
+\t\t}
+\t\th2{
+\t\t\t
+\t\t    -webkit-margin-before: 0.83em;
+\t\t    -webkit-margin-after: 0.83em;
+\t\t    -webkit-margin-start: 0px;
+\t\t    -webkit-margin-end: 0px;
+\t\t    text-align: center;
+\t\t    box-sizing: border-box;
+\t\t    font-size: 60px;
+\t\t    line-height: 60px;
+\t\t    font-weight: 700;
+\t\t    color: #fff;
+\t\t    text-decoration: none;
+\t\t    text-transform: uppercase;
+\t\t    text-shadow: -1px 0 1px #000;
+\t\t    background-color: transparent;
+\t\t    border-width: 0px;
+\t\t    border-color: #000;
+\t\t    border-style: none;
+\t\t    margin-top: 40px;
+\t\t    margin-bottom: 20px;
+\t\t    display: block;
+\t\t}
+\t\th4{
+\t\t\tfont-size: 18px;
+\t\t    color: #f7f7f7;
+\t\t    margin-bottom: 30px;
+\t\t    font-weight: 600;
+\t\t    text-shadow: 0px 1px 1px #000;
+\t\t    text-transform: uppercase;
+\t        margin-top: 10px;
+\t        line-height: 1.1;
+\t        box-sizing: border-box;
+            display: block;
+\t\t    -webkit-margin-before: 1.33em;
+\t\t    -webkit-margin-after: 1.33em;
+\t\t    -webkit-margin-start: 0px;
+\t\t    -webkit-margin-end: 0px;
+\t\t    text-align: center;
+\t\t}
+\t\t.center{
+\t\t\tposition: fixed;
+\t\t\ttop: 50%;
+\t\t\tleft: 50%;
+\t\t\twidth:50%;
+\t\t\theight: 50%;
+\t\t\t-webkit-transform: translateX(-50%) translateY(-50%);
+\t\t}
+    </style>
+</head>
+
+<body>
+\t<div class=\"center\">
+\t\t<h2>FAST MVC</h2>
+\t\t<h4>欢迎来到FAST MVC</h4>
+\t\t<h4>开始你的第一个应用</h4>
+\t</div>
+</body>
+
+</html>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "index.html";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+
+    public function getSource()
+    {
+        return "<!DOCTYPE html>
+<html lang=\"en\">
+
+<head>
+    <meta charset=\"utf-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <meta name=\"description\" content=\"\">
+    <meta name=\"author\" content=\"\">
+    <title>FAST首页</title>
+    <style>
+\t\tbody{
+\t\t\tbackground: rgba(220, 55, 55, 0.9);
+    \t\tbackground-size: 100% 100%;
+    \t\tfont-family: Trebuchet MS, snas-serif;
+\t\t}
+\t\th2{
+\t\t\t
+\t\t    -webkit-margin-before: 0.83em;
+\t\t    -webkit-margin-after: 0.83em;
+\t\t    -webkit-margin-start: 0px;
+\t\t    -webkit-margin-end: 0px;
+\t\t    text-align: center;
+\t\t    box-sizing: border-box;
+\t\t    font-size: 60px;
+\t\t    line-height: 60px;
+\t\t    font-weight: 700;
+\t\t    color: #fff;
+\t\t    text-decoration: none;
+\t\t    text-transform: uppercase;
+\t\t    text-shadow: -1px 0 1px #000;
+\t\t    background-color: transparent;
+\t\t    border-width: 0px;
+\t\t    border-color: #000;
+\t\t    border-style: none;
+\t\t    margin-top: 40px;
+\t\t    margin-bottom: 20px;
+\t\t    display: block;
+\t\t}
+\t\th4{
+\t\t\tfont-size: 18px;
+\t\t    color: #f7f7f7;
+\t\t    margin-bottom: 30px;
+\t\t    font-weight: 600;
+\t\t    text-shadow: 0px 1px 1px #000;
+\t\t    text-transform: uppercase;
+\t        margin-top: 10px;
+\t        line-height: 1.1;
+\t        box-sizing: border-box;
+            display: block;
+\t\t    -webkit-margin-before: 1.33em;
+\t\t    -webkit-margin-after: 1.33em;
+\t\t    -webkit-margin-start: 0px;
+\t\t    -webkit-margin-end: 0px;
+\t\t    text-align: center;
+\t\t}
+\t\t.center{
+\t\t\tposition: fixed;
+\t\t\ttop: 50%;
+\t\t\tleft: 50%;
+\t\t\twidth:50%;
+\t\t\theight: 50%;
+\t\t\t-webkit-transform: translateX(-50%) translateY(-50%);
+\t\t}
+    </style>
+</head>
+
+<body>
+\t<div class=\"center\">
+\t\t<h2>FAST MVC</h2>
+\t\t<h4>欢迎来到FAST MVC</h4>
+\t\t<h4>开始你的第一个应用</h4>
+\t</div>
+</body>
+
+</html>
+";
+    }
+}
