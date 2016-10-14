7a25eab52bb00c621c286fcaa0cfa1e0a0ff705d
hai <1614404051@qq.com>
2016-10-14 19:57:26 +0800

    version lhy 10-14-1

--- application/controllers/ListsController.php
+++ application/controllers/ListsController.php
@@ -15,6 +15,9 @@ class ListsController extends \core\imooc
 	//发单信息添加入库
 	public function add_lists()
 	{
+		//经纬度拆分
+		$s_mission = explode(',',post('mission'));
+		$s_finish  = explode(',',post('finish'));
 		// $u_id = 	$_SESSION['id'];
 		$data['u_id'] = 1;
 		$data['s_title'] = 		post('title');
@@ -26,8 +29,10 @@ class ListsController extends \core\imooc
 		$data['s_violate_money']=post('violate_money');
 		$data['s_address'] = 	post('address');
 		$data['s_end_address']=	post('end_address');
-		$data['s_mission'] =	post('mission');
-		$data['s_finish'] =		post('finish');
+		$data['s_m_lng'] =		$s_mission[0];
+		$data['s_m_lat'] =		$s_mission[1];
+		$data['s_f_lng'] =		$s_finish[0];
+		$data['s_f_lat'] =		$s_finish[1];
 		$lists = new listsModel();
 		$bool = $lists->add_lists($data);
 		var_dump($bool);
@@ -36,6 +41,40 @@ class ListsController extends \core\imooc
 	//接单首页
 	public function receive()
 	{
-		$this->display('receive.php');
+		$this->display('receive.php');	
+	}
+
+	//带距离首页列表
+	public function receive_list()
+	{
+		$array = array();
+		$distance = get('distance');
+		$lng2 = get('lng');
+		$lat2 = get('lat');
+		$lists = new listsModel();
+		$u_id=2;
+		$data = $lists->lists($u_id);
+		// print_r($data);die;
+		foreach($data as $key=>$val)
+		{
+			//将角度转为弧度
+			$radLat1=deg2rad($val['s_m_lat']);//deg2rad()函数将角度转换为弧度
+			$radLat2=deg2rad($lat2);
+			$radLng1=deg2rad($val['s_m_lng']);
+			$radLng2=deg2rad($lng2);
+			$a=$radLat1-$radLat2;
+			$b=$radLng1-$radLng2;
+			$s=2*asin(sqrt(pow(sin($a/2),2)+cos($radLat1)*cos($radLat2)*pow(sin($b/2),2)))*6378.137*1000;
+			$data[$key]['distance'] = substr($s,0,strpos($s,'.'));
+		}
+		foreach($data as $key=>$val)
+		{
+			if($val['distance']<=$distance)
+			{
+				$array[] = $val;
+			}
+		}
+		// print_r($array);
+		echo json_encode($array);
 	}
 }
--- application/views/receive.php
+++ application/views/receive.php
@@ -9,70 +9,110 @@
     <link rel="stylesheet" href="{{host}}public/css/style.css"/>
     <script src="{{host}}public/js/jquery.min.js"></script>
      <script src="{{host}}public/js/jquery.mobile-1.4.5.min.js"></script>
+     <script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=ERtymnt2XAAWdaDdLGE60jqk0pm4Q4kT"></script>
 </head>
 <body style="background-color: #f2f2f2;">
 	<div class="loading"><img src="{{host}}public/images/ajax-loader.gif"/></div>
 	<div data-role="page" class="index" id="index">
 		<header data-role="header">
-			<h3>赚不停</h3>
+			<h3>接单</h3>
 		</header>
 		<div class="ui-content" data-role="content"> 
-			<ul class="slick">
-			    <li><a href="#"><img src="{{host}}public/images/ban01.png"/></a></li>
-			    <li><a href="#"><img src="{{host}}public/images/ban02.png"/></a></li>
-			    <li><a href="#"><img src="{{host}}public/images/ban03.png"/></a></li>
-			    <li><a href="#"><img src="{{host}}public/images/ban04.png"/></a></li>
-			</ul>
-			<ul class="index-menu">
-				<li>
-					<img src="{{host}}public/images/icon01.png"/>
-					<p>保险承保</p>
-				</li>
-				<li>
-					<img src="{{host}}public/images/icon02.png"/>
-					<p>当日计息</p>
-				</li>
-				<li>
-					<img src="{{host}}public/images/icon03.png"/>
-					<p>续投加息</p>
-				</li>
-			</ul>
-			<dl class="index-year">
-				<a href="month.html" data-transition="slide" data-ajax="false">
-					<dd>
-						<h4>月月盈</h4>
-						<p><small>年化</small>6.0%起</p>
-					</dd>
-					<dt>支持续投，次月起年化收益增加0.1%</dt>
-				</a>
-			</dl>
-			<dl class="index-year">
-				<a href="month.html" data-transition="slide" data-ajax="false">
-					<dd>
-						<h4>季度丰</h4>
-						<p><small>年化</small>6.3%起</p>
-					</dd>
-					<dt>支持续投，次月起年化收益增加0.1%</dt> 
-				</a>
-			</dl>
-			<dl class="index-year">
-				<a href="month.html" data-transition="slide" data-ajax="false">
-					<dd>
-						<h4>半年享</h4>
-						<p><small>年化</small>6.6%起</p>
-					</dd>
-					<dt>支持续投，次月起年化收益增加0.1%</dt>
-				</a>
-			</dl>
+		<!-- 自定义样式 -->
+		 <!-- data-role='none' style="width:20px" -->
+		 	<div>
+		 		<div style="width:30%;float:left">
+				 	<select name="" id="distance" >
+						<option value="1000">1000m</option>
+						<option value="2000" selected>2000m</option>
+						<option value="3000">3000m</option>
+						<option value="5000">5000m</option>
+						<option value="10000">10000m</option>
+						<option value="15000">15000m</option>
+					</select>
+				</div>
+		 		<div style="width:65%;float:left;margin-top:11px;padding-left:5%">
+		 			当前位置：<br/><span  id="address"></span>
+		 		</div>
+		 	</div>
+			<div id="content">
+				<!-- <dl class="index-year" >
+					<a href="month.html" data-transition="slide" data-ajax="false">
+						<dd>
+							<div style="float:left;width:60%;">
+							<h4 rowspan='2'>帮我带个饭！！</h4>
+							</div>
+							<div style="float:left; width:40%">
+							<p style="float:none"><small>距离您：</small>1500m</p>
+							<p style="float:none"><small>用户星级：</small><img src="{{host}}public/images/star1.gif" width='80' alt=""></p>
+							</div>
+						</dd>
+						<dt><font color="#009dd9">李智渊</font>于2016-05-20 18:20:25发布</dt>
+					</a>
+				</dl> -->
+			</div>
+			<div id="allmap" style="display:none"></div>
 		</div>
-		<footer data-role="footer" data-position="fixed">  
-			<ul>
-				<li><a href="index.html"  rel="external">接单</a></li>
-				<li><a href="self.html"  rel="external">发单</a></li>
-				<li><a href="self.html"  rel="external">我的</a></li>
-			</ul>
-		</footer>
 		<script src="{{host}}public/js/slick.min.js" ></script>
+		<script>
+			$(function(){
+				// var distance = $("#distance").value()
+				// $.get('/lists/receive_list',{'distance':distance},function(msg){
+
+				// })
+			})
+		</script>
+		<script>  
+			$(function(){  
+				var lat;
+				var lng;
+				var map = new BMap.Map("allmap");
+				var point = new BMap.Point(116.331398,39.897445);
+				map.centerAndZoom(point,12);
+				var geolocation = new BMap.Geolocation();
+				geolocation.getCurrentPosition(function(r){
+					if(this.getStatus() == BMAP_STATUS_SUCCESS){
+						var mk = new BMap.Marker(r.point);
+						map.addOverlay(mk);
+						map.panTo(r.point);
+						lat=r.point.lat;
+						lng=r.point.lng;
+						$.get('http://api.map.baidu.com/geocoder/v2/?ak=ERtymnt2XAAWdaDdLGE60jqk0pm4Q4kT&callback=renderReverse&location='+r.point.lat+','+r.point.lng+'&output=json&pois=1',function(msg){
+							$("#address").append(msg.result.formatted_address)
+
+							//位置获取完成获取信息
+							var distance = $("#distance").val()
+							$.get("{{host}}lists/receive_list/distance/"+distance+"/lng/"+lng+"/lat/"+lat,function(msg){
+								var v='';
+								for(var i=0;i<msg.length;i++)
+								{
+									v=v+'<dl class="index-year" ><a href="month.html" data-transition="slide" data-ajax="false"><dd><div <h4 rowspan="2">'+msg[i].s_title+'</h4></div><div style="float:left; width:40%"><p style="float:none"><small>距离您：</small>'+msg[i].distance+'m</p><p style="float:none"><small>用户星级：</small><img src="{{host}}public/images/star1.gif" width="80" alt=""></p></div></dd><dt><font color="#009dd9">李智渊</font>于'+msg[i].s_time+'发布</dt></a></dl>'
+								}
+								$("#content").html(v)
+							},'json')
+						},'jsonp')
+						// alert('您的位置：'+r.point.lng+','+r.point.lat);
+					}
+					else {
+						alert('failed'+this.getStatus());
+					}        
+				},{enableHighAccuracy: true})
+
+				//判断距离
+				$("#distance").change(function(){
+					var distance = $(this).val()
+					$.get("{{host}}lists/receive_list/distance/"+distance+"/lng/"+lng+"/lat/"+lat,function(msg){
+						var v='';
+						for(var i=0;i<msg.length;i++)
+						{
+							v=v+'<dl class="index-year" ><a href="month.html" data-transition="slide" data-ajax="false"><dd><div <h4 rowspan="2">'+msg[i].s_title+'</h4></div><div style="float:left; width:40%"><p style="float:none"><small>距离您：</small>'+msg[i].distance+'m</p><p style="float:none"><small>用户星级：</small><img src="{{host}}public/images/star1.gif" width="80" alt=""></p></div></dd><dt><font color="#009dd9">李智渊</font>于'+msg[i].s_time+'发布</dt></a></dl>'
+						}
+						$("#content").html(v)
+					},'json')
+				})
+			})
+			
+		</script>
 	    <script type="text/javascript">
 	    	$(document).on("pagecreate","#index",function(){
  					$('.slick').slick({
@@ -81,7 +121,6 @@
 						    	autoplaySpeed:2000,
 						    	arrows:false,
 						    }); 
-					
 					});
 					$(window).load(function(){
 						$(".loading").fadeOut()
