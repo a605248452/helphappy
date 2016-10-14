7720c938732066e07c0ef3c48f9c984e356dd69a
Your Name <you@example.com>
2016-10-14 09:48:17 +0800

    v1.3 author ybg

--- core/config/database.php
+++ core/config/database.php
@@ -2,10 +2,10 @@
 return array(
     'database_type'=>'mysql',
     'database_name'=>'bbl',
-    'server'=>'114.215.142.111',
-    'username'=>'lzy',
-    'password'=>'lzy',
+    'server'=>'112.126.94.78',
+    'username'=>'project',
+    'password'=>'123456',
     'charset'=>'utf8',
     'port' => 3306,
-    'prefix' => 'b_'
+    'prefix' => ''
 );
