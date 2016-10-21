280a69732aa89ad04d2ba3e64398736d52479870
lzy <605248452@qq.com>
2016-10-21 15:26:21 +0800

    lzy

--- application/controllers/WechatController.php
+++ application/controllers/WechatController.php
@@ -7,28 +7,6 @@ class WechatController extends \core\imooc
 
     public function check()
     {
-        define("TOKEN", "bbl");
-        if (!defined("TOKEN")) {
-            throw new Exception('TOKEN is not defined!');
-        }
-
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];
-
-        $token = TOKEN;
-        $tmpArr = array($token, $timestamp, $nonce);
-        // use SORT_STRING rule
-        sort($tmpArr, SORT_STRING);
-        $tmpStr = implode( $tmpArr );
-        $tmpStr = sha1( $tmpStr );
-
-        if( $tmpStr == $signature ){
-            $echoStr = $_GET["echostr"];
-            echo $echoStr;
-            exit;
-        }else{
-            return false;
-        }
+        echo 1;
     }
 }
