14b9450cf8bc6c20362eccf82d10abcf87491996
Your Name <you@example.com>
2016-10-21 12:00:57 +0800

    delete log

--- log/2016102008/log.php
+++
@@ -1,3 +0,0 @@
-2016-10-20 08:23:52"Controller:Index       Action:index"
-2016-10-20 08:24:22"Controller:Index       Action:index"
-2016-10-20 08:24:41"Controller:Index       Action:index"
--- log/2016102013/log.php
+++
@@ -1,4 +0,0 @@
-2016-10-20 13:55:06"Controller:Index       Action:index"
-2016-10-20 13:55:07"Controller:Index       Action:index"
-2016-10-20 13:55:09"Controller:Index       Action:index"
-2016-10-20 13:59:55"Controller:Login       Action:reg"
