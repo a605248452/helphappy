59480549293afb1b873e05e85ed035e0dfd22632
lzy <605248452@qq.com>
2016-10-21 15:11:57 +0800

    v2.4.8 author lzy

--- application/controllers/WechatController.php
+++ application/controllers/WechatController.php
@@ -2,15 +2,33 @@
 
 namespace application\controller;
 
-use core\lib\wechatCallbackapiTest;
-
 class WechatController extends \core\imooc
 {
 
     public function check()
     {
         define("TOKEN", "bbl");
-        $wechatObj = new wechatCallbackapiTest();
-        $wechatObj->valid();
+        if (!defined("TOKEN")) {
+            throw new Exception('TOKEN is not defined!');
+        }
+
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        // use SORT_STRING rule
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){
+            $echoStr = $_GET["echostr"];
+            echo $echoStr;
+            exit;
+        }else{
+            return false;
+        }
     }
 }
